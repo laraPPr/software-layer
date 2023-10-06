ac9890dd43bd7f02dbaf4d92fc8b9871d2e3f099
vsc46128 vscuser <vsc46128@gligar08.gastly.os>
2023-10-06 14:38:58 +0200

    set up easybuild

--- bot/test-build-vsc.sh
+++ bot/test-build-vsc.sh
@@ -31,8 +31,9 @@ echo_green "All set, let's start installing some software with EasyBuild Dev in 
 for es in $(ls vsc-*.yml); do
     if [ -f ${es} ]; then
         echo_green "checking easybuild version"
-        ml
-        export EB_VERBOSE=1
+        export PYTHONPATH=$HOME/.local/lib/python3.8/site-packages/:$PYTHONPATH
+        export EB_PYTHON=python3.8
+        echo "using local pip install of easybuild with python3.8"
         ${EB} --version
 
         echo_green "Feeding easystack file ${es} to EasyBuild..."
