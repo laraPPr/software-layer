d8ee92cebadffb70e928e8a20fee80bf75bb0af8
Lara Ramona Peeters <49882639+laraPPr@users.noreply.github.com>
2023-10-06 11:39:38 +0200

    Create test-build-vsc.sh

---
+++ bot/test-build-vsc.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+#
+# script to build the software for VSC. Intended use is that it is called
+# by a (batch) job running on a compute node.
+#
+# This script is based on a script that is part of the EESSI software layer, 
+# written by Thomas Roeblitz (@trz42), 
+# see https://github.com/EESSI/software-layer.git
+#
+# license: GPLv2
+#
+# author: Lara Peeters (laraPPr)
+
+# ASSUMPTIONs:
+#  - working directory has been prepared by the bot with a checkout of a
+#    pull request (OR by some other means)
+#  - the working directory contains a directory 'cfg' where the main config
+#    file 'job.cfg' has been deposited
+#  - the directory may contain any additional files referenced in job.cfg
+
+# stop as soon as something fails
+set -e
+
+
+echo_green "All set, let's start installing some software with EasyBuild Dev in ${EASYBUILD_INSTALLPATH}
+
+for es in $(ls vsc-*.yml); do
+  echo ${es}
+
