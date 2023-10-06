d1f95b2db5f26a08e6570bea4dbf05231b739ec0
vsc46128 vscuser <vsc46128@gligar08.gastly.os>
2023-10-06 14:17:03 +0200

    check PATH environment

--- bot/test-build-vsc.sh
+++ bot/test-build-vsc.sh
@@ -31,7 +31,7 @@ echo_green "All set, let's start installing some software with EasyBuild Dev in 
 for es in $(ls vsc-*.yml); do
     if [ -f ${es} ]; then
         echo_green "checking easybuild version"
-        env | sort PATH
+        env | grep PATH
         ${EB} --version
 
         echo_green "Feeding easystack file ${es} to EasyBuild..."
