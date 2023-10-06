08fe900f0d8e2b93a24af2376c2f470498d9469b
vsc46128 vscuser <vsc46128@gligar08.gastly.os>
2023-10-06 14:04:13 +0200

    check easybuild

--- bot/test-build-vsc.sh
+++ bot/test-build-vsc.sh
@@ -30,6 +30,9 @@ echo_green "All set, let's start installing some software with EasyBuild Dev in 
 
 for es in $(ls vsc-*.yml); do
     if [ -f ${es} ]; then
+        echo_green "checking easybuild version"
+        ${EB} --version
+
         echo_green "Feeding easystack file ${es} to EasyBuild..."
         ${EB} --easystack ${es} --robot
         #ec=$?
