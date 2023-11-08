ac77f3548eb88483c062458bb72d1bf412e46e17
Lara Ramona Peeters <49882639+laraPPr@users.noreply.github.com>
2023-11-08 16:01:27 +0100

    Update test-build-vsc.sh

--- bot/test-build-vsc.sh
+++ bot/test-build-vsc.sh
@@ -29,10 +29,6 @@ EB='eb --detect-loaded-modules=purge'
 
 echo_green "All set, let's start installing some software with EasyBuild Dev in ${EASYBUILD_INSTALLPATH}..."
 
-env | grep EASYBUILD
-env | grep SLURM
-env | grep VSC
-
 ${EB} --show-config
 
 for es in $(ls vsc-*.yml); do
